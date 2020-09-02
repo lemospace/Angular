c2e5a7550d385f04c3673f81e301226ae858dbb0
lemospace <thelemospace@gmail.com>
2020-09-02 14:51:55 +0300

    commit

--- Task 4.7/my-first-project/src/app/app-routing.module.ts
+++ Task 4.7/my-first-project/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PreloadAllModules } from '@angular/router';
 import { BikesComponent } from './bikes/bikes.component';
 import { BikeDetailComponent } from './bike-detail/bike-detail.component';
 import { OrderCompleteComponent } from './order-complete/order-complete.component';
@@ -11,13 +12,27 @@ const routes: Routes = [
   { path: 'bikes', component: BikesComponent },
   { path: 'detail/:id', component: BikeDetailComponent },
   { path: 'complete', component: OrderCompleteComponent },
-  { path: 'order', component: OrderComponent },
+  //{ path: 'order', component: OrderComponent },
   { path: 'contact-complete', component: ContactCompleteComponent },
   { path: 'main', component: MainComponent },
+  {
+    path: 'order',
+    loadChildren: () =>
+      import('./order/order.module').then((m) => m.OrderModule),
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
